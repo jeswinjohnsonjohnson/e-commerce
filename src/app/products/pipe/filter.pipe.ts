4f532d36c8edac931b593b6faaa8e848f9b6e4e7
jeswinjohnsonjohnson <79085829+jeswinjohnsonjohnson@users.noreply.github.com>
2022-11-06 01:24:24 +0530

    initial

---
+++ src/app/products/pipe/filter.pipe.ts
@@ -0,0 +1,25 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+
+  transform(value: any[], filterstring: string, propName: string): any {
+
+    const result: any = []
+    if (!value || filterstring === '' || propName === '') {
+      return value
+    }
+
+    value.forEach((item: any) => {
+      if (item[propName].trim().toLowerCase().includes(filterstring.toLowerCase())) {
+        result.push(item)
+      }
+
+    })
+
+    return result
+  }
+
+}
